28217cb5ddb18aae342a806757174629d683fb2c
John Doe <47755918+Rocketblaster247@users.noreply.github.com>
2019-05-19 16:17:18 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-document.write("Hey!");
+var prnt = '';
+var println = function (a) {
+  prnt += a;
+  prnt += "<br>";
+};
+var printAll = function () {
+  document.write(prnt);
+};
+
+var src = document.location.toString().split("?url=")[1];
+if (!src || src === '') {
+  println("No page to load.");
+  printAll();
+  return;
+}
+
+println("Loading \"" + src + "\"...");
+
+var xhr = new XMLHttpRequest();
+xhr.open(src);
+xhr.addEventListener("load", function () {
+  println("Loaded!");
+});
+xhr.addEventListener("error", function () {
+  println("Error!");
+});
+printAll();
